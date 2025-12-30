6bb055541e8ac286fef07c455f72e2d3cf972875
Edem Ziddah Kwame <contact@codevoweb.com>
2025-12-30 20:04:31 +0000

    feat(web): setup Tailwind CSS v4 and shadcn UI for Vue

--- apps/web/app/app.vue
+++ apps/web/app/app.vue
@@ -1,6 +1,16 @@
+<script setup lang="ts">
+import { Alert, AlertDescription, AlertTitle } from './components/ui/alert';
+import { Button } from './components/ui/button';
+</script>
+
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <Button variant="outline"> Button </Button>
+    <Alert>
+      <AlertTitle>Heads up!</AlertTitle>
+      <AlertDescription>
+        You can add components and dependencies to your app using the cli.
+      </AlertDescription>
+    </Alert>
   </div>
 </template>
